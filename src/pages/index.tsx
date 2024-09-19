d84a76df612e22c68c89d664e74202539d39a47f
Mrinal Prakash <mrinalprakash.collegedunia@gmail.com>
2024-09-19 18:26:22 +0530

    Updated Meta Tags for SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -234,14 +234,32 @@ export default function Home() {
     <>
       <Head>
         <title>Masite: A Portfolio Website Builder</title>
-        <meta name="description" content="Create your own portfolio website effortlessly using Masite, with customizable designs and features tailored for developers." />
-        <meta name="keywords" content="portfolio builder, web development, create portfolio, customizable designs" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Masite: A Portfolio Website Builder" />
-        <meta property="og:description" content="Create your own portfolio website effortlessly using Masite" />
+
+        {/* MS, fb & WhatsApp */}
+
+        {/* MS Tile - for Microsoft apps */}
+        <meta name="msapplication-TileImage" content="https://raw.githubusercontent.com/emphay/masite-portfolio-website-builder/main/public/img/sitedesign.png" />
+
+        {/* fb & WhatsApp */}
+
+        {/* Site Name, Title, and Description to be displayed */}
+        <meta property="og:site_name" content="Masite: A Portfolio Website Builder" />
+        <meta property="og:title" content="Build Your Portfolio with Masite" />
+        <meta property="og:description" content="Effortlessly create and customize your personal portfolio website using Masite. Show off your work and projects with ease." />
+
+        {/* Image to display */}
         <meta property="og:image" content="https://raw.githubusercontent.com/emphay/masite-portfolio-website-builder/main/public/img/sitedesign.png" />
-        <meta property="og:url" content="https://masite-portfolio-website-builder.vercel.app/" />
+
+        {/* No need to change anything here */}
         <meta property="og:type" content="website" />
+        <meta property="og:image:type" content="image/jpeg" />
+
+        {/* Size of image. Any size up to 300. Anything above 300px will not work in WhatsApp */}
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+
+        {/* Website to visit when clicked in fb or WhatsApp */}
+        <meta property="og:url" content="https://masite-portfolio-website-builder.vercel.app/" />
       </Head>
 
       <Layout style={{ backgroundColor: designData.backgroundColor }} className={styles.lay} >
